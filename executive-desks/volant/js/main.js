fb008e2fcec97f99d2f8cd91e8aba8a26f50ca93
aneeban10 <aneeban10@gmail.com>
2018-04-01 03:39:17 +0530

    Add files via upload

--- executive-desks/volant/js/main.js
+++ executive-desks/volant/js/main.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $(".menu").click(function(){
     $(this).toggleClass("is-active");
   });
+  $("#outsidediv").click(function(){
+    $(".menu").toggleClass("is-active");
+  });
 });
